f4bd02ab929afc7ec9fbf493fabf349c402f8b9e
MohammadHasan <mhaminii18@gmail.com>
2023-04-01 20:28:43 +0330

    add exercise d answer

---
+++ exercise_d/target/debug/deps/exercise_d-806c0a2f8965462b.d
@@ -0,0 +1,5 @@
+/home/mohammadhasan/MyCodes/LearnRust/exercise_d/target/debug/deps/exercise_d-806c0a2f8965462b: src/main.rs
+
+/home/mohammadhasan/MyCodes/LearnRust/exercise_d/target/debug/deps/exercise_d-806c0a2f8965462b.d: src/main.rs
+
+src/main.rs:
---
+++ exercise_d/target/debug/exercise_d.d
@@ -0,0 +1,1 @@
+/home/mohammadhasan/MyCodes/LearnRust/exercise_d/target/debug/exercise_d: /home/mohammadhasan/MyCodes/LearnRust/exercise_d/src/main.rs
