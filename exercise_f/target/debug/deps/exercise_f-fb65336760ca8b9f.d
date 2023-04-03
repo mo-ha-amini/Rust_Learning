a06b26382984b7e66f606b94de4cbd68251987ba
MohammadHasan <mhaminii18@gmail.com>
2023-04-03 21:26:28 +0330

    add exercise f answer

---
+++ exercise_f/target/debug/deps/exercise_f-fb65336760ca8b9f.d
@@ -0,0 +1,5 @@
+/home/mohammadhasan/MyCodes/LearnRust/exercise_f/target/debug/deps/exercise_f-fb65336760ca8b9f: src/main.rs
+
+/home/mohammadhasan/MyCodes/LearnRust/exercise_f/target/debug/deps/exercise_f-fb65336760ca8b9f.d: src/main.rs
+
+src/main.rs:
---
+++ exercise_f/target/debug/exercise_f.d
@@ -0,0 +1,1 @@
+/home/mohammadhasan/MyCodes/LearnRust/exercise_f/target/debug/exercise_f: /home/mohammadhasan/MyCodes/LearnRust/exercise_f/src/main.rs
